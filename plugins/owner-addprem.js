92a45fc2edcc249013a0dde0b3c4baeb0eb7ddda
hamzaziti <160261690+hamzaziti@users.noreply.github.com>
2024-03-16 11:34:54 +0000

    Update owner-addprem.js

--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -59,7 +59,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   }
 };
 handler.help = ['addprem [@user] <days>'];
-handler.tags = ['owner'];
+handler.tags = ['OWNER MENU'];
 handler.command = ['addprem', 'userpremium', 'addprem2', 'userpremium2', 'addprem3', 'userpremium3', 'addprem4', 'userpremium4'];
 handler.group = true;
 handler.rowner = true;
